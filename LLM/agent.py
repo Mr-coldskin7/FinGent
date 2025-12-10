166224da6fafb08aa3a46afd10c604f4b428ba93
coldskin7 <3020605993@qq.com>
2025-12-10 19:35:18 +0800

    add vue framework and some chinese stocks'api

---
+++ LLM/agent.py
@@ -0,0 +1,34 @@
+import langchian
+# a class to build analyisis agents to analyze stock
+# I will build two main agents
+#===========================================================================================================
+#
+# Fist agent: stock analysis master
+# This agent possesses reverse-thinking capabilities and primarily focuses on the strategic interplay between 
+# institutional players (market manipulators) and retail investors as reflected in technical indicators. 
+# He has a clear understanding of the following principles:
+# Institutional players, with their substantial capital, generally do not want retail investors to profit.
+# Typically, before initiating a price rally, they deliberately create market volatility to force retail investors 
+# to exit their positions, after which they drive the price upward. 
+# Once the rally is underway and technical indicators show stable and positive momentum, 
+# they trap retail investors into holding losing positions.
+# He never chases high prices or panics into selling during dips.
+# Instead, he acts with boldness yet meticulous caution, 
+# demonstrating strong psychological resilience and a comprehensive, big-picture perspective.
+# This agent is focuse on the emotional aspects of stock market behavior.
+#
+#===========================================================================================================
+#
+# Second agent: Morefit
+# This agent’s strategy focuses on stock fundamentals and the companies themselves. 
+# Like the early Warren Buffett, he seeks out undervalued stocks and boldly accumulates them. 
+# His core approach emphasizes the intrinsic value of businesses over market speculation.
+# He pays close attention to corporate financial statements, analyzes the narratives behind those reports, 
+# and then—by understanding the company’s operations—breaks its business down into smaller, 
+# relatable "mini-businesses" to estimate what the stock’s fair price should be. 
+# Through this method, he identifies potentially undervalued stocks.
+#
+#===========================================================================================================
+
+class Agent:
+    pass
